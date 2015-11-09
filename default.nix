5ba2312f4f1180c2fea478ca3b533bfd7faaf70a
Daniel Bergey <bergey@teallabs.org>
2015-11-08 22:31:27 -0500

    add default.nix
    hsnippet will use this, if no one else does

---
+++ default.nix
@@ -0,0 +1,20 @@
+{ mkDerivation, base, base64-bytestring, bytestring, colour
+, containers, diagrams-core, diagrams-lib, directory, filepath
+, hashable, JuicyPixels, lens, monoid-extras, mtl, old-time
+, optparse-applicative, process, reflex, reflex-dom
+, reflex-dom-contrib, semigroups, split, stdenv, text, time
+}:
+mkDerivation {
+  pname = "diagrams-reflex";
+  version = "0.1";
+  src = ./.;
+  build-depends = [
+    base base64-bytestring bytestring colour containers diagrams-core
+    diagrams-lib directory filepath hashable JuicyPixels lens
+    monoid-extras mtl old-time optparse-applicative process reflex
+    reflex-dom reflex-dom-contrib semigroups split text time
+  ];
+  homepage = "http://projects.haskell.org/diagrams/";
+  description = "reflex backend for diagrams drawing EDSL";
+  license = stdenv.lib.licenses.bsd3;
+}
